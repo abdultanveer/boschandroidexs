fc6ab6ff95f4c596259aca76bc8c5c4dd0ad3dea
abdul <abdul.tanveer@gmail.com>
2024-11-19 14:59:15 +0530

    higher order functions

--- app/src/main/java/com/example/bosch/kotlinexs/FnDemo.kt
+++ app/src/main/java/com/example/bosch/kotlinexs/FnDemo.kt
@@ -2,6 +2,12 @@ package com.example.bosch.kotlinexs
 val upperCase1: (String) -> String = { str: String -> str.uppercase() } // 1
 
 fun main() {
+    var a :Int = 20
+    val enc1: (String) -> String = { input -> input.toUpperCase() }
+   println( encodeMsg("abdul",enc1))
+
+    println(encodeMsg("RACE-CAR",::enc2))
+    var wl:String = "abdul"
     println(upperCase1("hello"))
     add(c=60, a= 10)
     add(10,c=30)
@@ -9,6 +15,9 @@ fun main() {
     println("hello world")
     printHello("abdul")   //abdul = argument
 }
+
+fun enc2(input:String): String = input.reversed()
+
 
 fun convertToUpperCase(name: String):String{
     return  name.uppercase()
@@ -26,4 +35,11 @@ fun add(a:Int, b:Int = 20, c:Int){
 fun drive(speed: String = "fast") {
     println("driving $speed")
 }
+
+fun encodeMsg(msg: String,
+              encode: (String) -> String): String {
+
+    return encode(msg)
+}
+
 
