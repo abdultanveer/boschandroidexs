0686f94b2bf0f8e0657eb619b18481f0d1c4dd24
abdul <abdul.tanveer@gmail.com>
2024-11-21 10:50:46 +0530

    sms broadcast receiver - smsbody n phno

--- app/src/main/java/com/example/bosch/SmsReceiver.kt
+++ app/src/main/java/com/example/bosch/SmsReceiver.kt
@@ -3,12 +3,17 @@ package com.example.bosch
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.provider.Telephony
 import android.util.Log
 
 class SmsReceiver : BroadcastReceiver() {
 var TAG = SmsReceiver::class.java.simpleName
 
     override fun onReceive(context: Context, intent: Intent) {
-      Log.i(TAG,"abdul received sms --bosch")
+
+        val message = Telephony.Sms.Intents.getMessagesFromIntent(intent)
+        val content =  message[0].displayMessageBody
+
+      Log.i(TAG,"abdul bosch"+ content + message[0].originatingAddress)
     }
 }
