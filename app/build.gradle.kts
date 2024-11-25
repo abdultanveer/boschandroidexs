26a2d00d4453cab8ebecc2ff89aa0dcf049a1db1
abdul <abdul.tanveer@gmail.com>
2024-11-25 12:02:31 +0530

    conect to internet -2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.retrofit)
+    implementation(libs.converter.scalars)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+  //  implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+
 }
--- app/src/main/java/com/example/bosch/InternetActivity.kt
+++ app/src/main/java/com/example/bosch/InternetActivity.kt
@@ -1,14 +1,19 @@
 package com.example.bosch
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.bosch.databinding.ActivityInternetBinding
 import com.example.bosch.databinding.ActivityMainBinding
+import com.example.bosch.internet.MarsApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class InternetActivity : AppCompatActivity() {
+    var TAG = InternetActivity::class.java.simpleName
 lateinit var binding: ActivityInternetBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +22,17 @@ lateinit var binding: ActivityInternetBinding
         setContentView(view)
 
         binding.btnJson.setOnClickListener {
-            getJson()
+            getMarsPhotos()
         }
 
     }
 
-    private fun getJson() {
+    private fun getMarsPhotos() {
+        GlobalScope.launch {
+            val listResult = MarsApi.retrofitService.getPhotos()
+            Log.i(TAG,listResult.toString())
+        }
 
     }
+
 }
