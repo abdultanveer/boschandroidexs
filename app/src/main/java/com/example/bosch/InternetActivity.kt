8686ce8e5cff165b143566cba2167cca071bc4d4
abdul <abdul.tanveer@gmail.com>
2024-11-26 10:23:13 +0530

    get n put data into firestore

--- app/src/main/java/com/example/bosch/InternetActivity.kt
+++ app/src/main/java/com/example/bosch/InternetActivity.kt
@@ -9,6 +9,8 @@ import coil.load
 import com.example.bosch.databinding.ActivityInternetBinding
 import com.example.bosch.internet.MarsApi
 import com.example.bosch.internet.MarsPhoto
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -18,6 +20,7 @@ class InternetActivity : AppCompatActivity() {
     var TAG = InternetActivity::class.java.simpleName
     lateinit var adapter:WordListAdapter
     lateinit var listPhotos:List<MarsPhoto>
+    val db = Firebase.firestore
 
 lateinit var binding: ActivityInternetBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +38,46 @@ lateinit var binding: ActivityInternetBinding
         }
 
     }
+
+
+    override fun onStart() {
+        super.onStart()
+        binding.btnSend.setOnClickListener { sendFirestore() }
+        binding.btnGet.setOnClickListener { getFireStore() }
+    }
+
+    private fun getFireStore() {
+        db.collection("bosch")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
+    }
+
+    private fun sendFirestore() {
+// Create a new user with a first and last name
+        val user = hashMapOf(
+            "first" to "vignesh",
+            "last" to "mudassir",
+            "born" to 1815
+        )
+
+// Add a new document with a generated ID
+        db.collection("bosch")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+    }
+
 
     private fun getMarsPhotos() {
         val coroutineExceptionHandler = CoroutineExceptionHandler{_, throwable ->
