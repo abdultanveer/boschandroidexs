b803dac4b2789c5266b5f9a800a346493bb503f9
abdul <abdul.tanveer@gmail.com>
2024-12-02 10:31:34 +0530

    manual espresso ui test-1

--- app/src/androidTest/java/com/example/bosch/MainActivityUiTest.kt
+++ app/src/androidTest/java/com/example/bosch/MainActivityUiTest.kt
@@ -1,5 +1,12 @@
 package com.example.bosch
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -21,8 +28,12 @@ class MainActivityUiTest {
     @Test
     fun loginTest() {
         //enter abdul android in etphone
-
+        onView(withId(R.id.etPhone))
+            .perform(typeText("abdul android"), closeSoftKeyboard());
         //click btnAlarm
+        onView(withId(R.id.btnAlarm)).perform(click());
         //check if tvMain has abdul android
+        onView(withId(R.id.tvMain))
+            .check(matches(withText("abdul android")));
     }
 }
